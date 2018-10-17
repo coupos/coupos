45f643dc8ab4aadc5681b181e8a684cd181fe7dd
Dozi0116 <oi.dozi0116@gmail.com>
2018-10-17 18:00:53 +0900

    Update rds_config.py

--- lambda/api/analyses/sale/GET/rds_config.py
+++ lambda/api/analyses/sale/GET/rds_config.py
@@ -1,4 +1,4 @@
-db_username = 'couposadmin'
-db_password = 'DCbX6pmeEcSC9$y!ks*P3K67GcCQoTuI'
-db_name = 'coupos_data'
-db_host = 'coupos.cd4yws3i9vwg.us-east-1.rds.amazonaws.com'
+db_username = 'COUPOS_DB_USERNAME'
+db_password = 'COUPOS_DB_PASSWORD'
+db_name = 'COUPOS_DB_NAME'
+db_host = 'COUPOS_DB_HOST'
